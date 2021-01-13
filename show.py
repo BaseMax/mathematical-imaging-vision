90de01d309a2dc3257b52542c5f65afd8f85b6ea
Max Base <MaxBaseCode@Gmail.Com>
2021-01-14 01:10:17 +0330

    Update show.py

--- show.py
+++ show.py
@@ -11,3 +11,31 @@ colors = [
     (0,0,0),
     (250,250,250)
 ]
+
+def close_color(pixel, colors):
+    close = (0, 0, 0) # default color
+    minDiff = 1000
+    for color in colors:
+
+        # color RGB
+        r = color[0]
+        g = color[1]
+        b = color[2]
+
+        # pixel RGB
+        pr =pixel[0]
+        pg = pixel[1]
+        pb = pixel[2]
+
+        # diff RGB
+        dr = r - pr
+        dg = g - pg
+        db = b - pb
+
+        sum = dr**2 + dg**2 + dg**2
+        diff = sqrt(sum)
+        if(diff < minDiff):
+            minDiff = diff
+            close = color
+
+    return close
