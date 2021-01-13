55d131a794b978f1f3ea193487132d92b7bf22fc
Max Base <MaxBaseCode@Gmail.Com>
2021-01-14 01:10:06 +0330

    Update show.py

--- show.py
+++ show.py
@@ -2,3 +2,12 @@
 # 2021-01-14
 # https://github.com/BaseMax/mathematical-imaging-vision
 
+from PIL import Image
+from math import sqrt
+
+# output color sets
+colors = [
+    # r,g,b
+    (0,0,0),
+    (250,250,250)
+]
